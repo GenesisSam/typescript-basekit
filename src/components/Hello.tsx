0350d798bb275654ff102c23777d0078bd739a94
genesissam <samuel940401@gamil.com>
2017-02-15 00:13:12 +0900

    Added SignUp, SignIn function

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,128 @@
 import * as React from "react";
+import * as firebase from "firebase";
 
-export default class Hello extends React.Component<{}, {}> {
+
+interface IHelloState {
+  userEmail: string;
+  userPassword: string;
+  CurrentUserAuth: any;
+}
+
+export default class Hello extends React.Component<{}, IHelloState> {
+  public constructor(props: any) {
+    super(props);
+    const fbDB = this.initFirebaseDB();
+
+    const user = firebase.auth().currentUser;
+
+    this.state = {
+      userEmail: "",
+      userPassword: "",
+      CurrentUserAuth: user,
+    };
+  }
+
   public componentDidMount() {
-    console.log("componentDidMount");
+    
   }
 
   public render() {
-    return (
-      <h1>HELLO WORLD!!</h1>
+    const { CurrentUserAuth } = this.state;
+
+    let userStateForm = (
+      <form>
+        <input name="email" type="email" placeholder="User email" onChange={this.handleEmailText.bind(this)} required/>
+        <input name="password" type="password" placeholder="Password" onChange={this.handlePasswordText.bind(this)} required/>
+        <br/>
+        <input type="submit" value="로그인" onClick={this.handleOnSubmit.bind(this)}/>
+        <input type="button" value='회원가입' onClick={this.handleOnSubmitSignUp.bind(this)}/>
+      </form>
     );
+
+    if (CurrentUserAuth) {
+      userStateForm = CurrentUserAuth.map((profile: any) => {
+        return (
+          <div>
+            Email: {profile.email}<br/>
+            Name: {profile.displayName}<br/>
+            UID: {profile.uid}
+          </div>
+        );
+      });
+    }
+
+    return (
+      <div>
+        <h1>HELLO WORLD!!</h1>
+        {userStateForm}
+      </div>
+    );
+  }
+
+  private initFirebaseDB() {
+    const config = {
+      
+    };
+
+    firebase.initializeApp(config);
+    return firebase.database();
+  }
+
+  private handleEmailText(e: any) {
+    this.setState({
+      userEmail: e.target.value,
+    });
+  }
+
+  private handlePasswordText(e: any) {
+    this.setState({
+      userPassword: e.target.value,
+    })
+  }
+
+  private handleOnSubmit(e: any) {
+    e.preventDefault();
+    const { userEmail, userPassword } = this.state;
+    // const credential = firebase.auth.EmailAuthProvider.credential(userEmail, userPassword);
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then((res) => {
+      this.setState({
+        CurrentUserAuth: res.providerData
+      });
+      
+    }).catch((err) => {
+      const errorCode = (err as any).code;
+      const errorMSG = err.message;
+
+      if (errorCode === "auth/wrong-password") {
+        alert("Wrong password");
+      } else {
+        alert(errorMSG);
+      }
+
+      console.log(err);
+      
+    });
+  }
+
+  private handleOnSubmitSignUp(e: any) {
+    e.preventDefault();
+    const { userEmail, userPassword } = this.state;
+    // const credential = firebase.auth.EmailAuthProvider.credential(userEmail, userPassword);
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).then((res) => {
+      this.setState({
+        CurrentUserAuth: res.providerData
+      });
+      
+    }).catch((err) => {
+      const errorCode = (err as any).code;
+      const errorMSG = err.message;
+
+      if (errorCode === "auth/weak-password") {
+        alert("Weak password");
+      } else {
+        alert(errorMSG);
+      }
+      console.log(err);
+    });
   }
 }
